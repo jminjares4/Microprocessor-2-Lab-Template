f16c3cfbccda5d3e989f68faa3492e6de3d68526
jminjares4 <minjares.jesus.ths@gmail.com>
2022-08-17 08:32:17 -0600

    Added esp-idf check

--- Lab_2/main/led.c
+++ Lab_2/main/led.c
@@ -4,75 +4,85 @@
  * @brief Custom LED driver source file
  * @version 0.1
  * @date 2022-07-25
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #include "driver/led.h"
 #include "driver/gpio.h"
+#include "esp_idf_version.h"
 
-#define LED_LOW     0  /*!< LED low logic */
-#define LED_HIGH    1  /*!< LED high logic */
+#define LED_LOW 0  /*!< LED low logic */
+#define LED_HIGH 1 /*!< LED high logic */
 
 /**
- * @brief LED enable 
- * 
+ * @brief LED enable
+ *
  * @param led pointer to LED object
- * @note The function assume that the user has initialize the members 
+ * @note The function assume that the user has initialize the members
  *       of the object.
  * @return None
  */
-void led_enable(led_t * const led){
+void led_enable(led_t *const led)
+{
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
     /* Select gpio pin */
     esp_rom_gpio_pad_select_gpio(led->pin);
+#else
+    gpio_pad_select_gpio(led->pin);
+#endif
     /* Set direction */
     gpio_set_direction(led->pin, GPIO_MODE_OUTPUT);
     /* Set level */
-    gpio_set_level(led->pin, LED_LOW );
+    gpio_set_level(led->pin, LED_LOW);
     /* Update state */
-    led->state = (state_t) OFF;
+    led->state = (state_t)OFF;
 }
 
 /**
  * @brief Turn on LED object pin
- * 
+ *
  * @param led pointer to LED object
  * @note  Function will update the state of LED
  * @return None
  */
-void led_on(led_t * const led){
+void led_on(led_t *const led)
+{
     gpio_set_level(led->pin, LED_HIGH);
     led->state = ON;
 }
 
 /**
  * @brief Turn off LED object pin
- * 
+ *
  * @param led pointer to LED object
  * @note  Function will update the state of LED
  * @return None
  */
-void led_off(led_t * const led){
+void led_off(led_t *const led)
+{
     gpio_set_level(led->pin, LED_LOW);
     led->state = OFF;
 }
 
 /**
  * @brief Toggle LED object pin
- * 
+ *
  * @param led pointer to LED object
  * @note  Function will update the state of LED
  * @return None
  */
-void led_toggle(led_t * const led){
-    switch(led->state){
-        case OFF:
-            led_on(led);
-            break;
-        case ON:
-            led_off(led);
-            break;
-        default:
+void led_toggle(led_t *const led)
+{
+    switch (led->state)
+    {
+    case OFF:
+        led_on(led);
+        break;
+    case ON:
+        led_off(led);
+        break;
+    default:
     }
 }
